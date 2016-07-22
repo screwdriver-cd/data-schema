fb37abca80394f2e979c516a3ebb1acb470f397c
Dao Lam <daolam112@gmail.com>
2016-07-22 15:59:58 -0700

    Expose plugins

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const models = require('./models');
 const api = require('./api');
 const config = require('./config');
+const plugins = require('./plugins');
 
-module.exports = { models, api, config };
+module.exports = { models, api, config, plugins };
