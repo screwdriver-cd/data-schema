c9fc6df7774030b39f2091d97db3b888b84e88c1
St. John Johnson <st.john.johnson@gmail.com>
2016-07-27 09:17:38 -0700

    Removing Platforms until we come up with the Template design

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@
 const build = require('./build');
 const job = require('./job');
 const pipeline = require('./pipeline');
-const platform = require('./platform');
 const user = require('./user');
 
-module.exports = { build, job, pipeline, platform, user };
+module.exports = { build, job, pipeline, user };
