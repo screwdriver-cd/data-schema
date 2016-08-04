531420d2019935eb53f6cc94035eb450eb224bfc
Noah Katzman <nkatzman@yahoo-inc.com>
2016-08-04 11:39:42 -0700

    api validation for the build webhooks plugin

--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@
 
 const pagination = require('./pagination');
 const validator = require('./validator');
+const webhooks = require('./webhooks');
 
-module.exports = { pagination, validator };
+module.exports = { pagination, validator, webhooks };
