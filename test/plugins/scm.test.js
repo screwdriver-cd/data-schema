c751e00a648ed771d26ac23704ae5484337768a4
Noah Katzman <nkatzman@yahoo-inc.com>
2016-08-15 17:23:58 -0700

    adding in schema validation for the scm-base plugin

---
+++ test/plugins/scm.test.js
@@ -0,0 +1,36 @@
+'use strict';
+const assert = require('chai').assert;
+const scm = require('../../plugins/scm');
+const validate = require('../helper').validate;
+
+describe('scm test', () => {
+    describe('getPermissions', () => {
+        it('validates', () => {
+            assert.isNull(validate('scm.getPermissions.yaml', scm.getPermissions).error);
+        });
+
+        it('fails', () => {
+            assert.isNotNull(validate('empty.yaml', scm.getPermissions).error);
+        });
+    });
+
+    describe('getCommitSha', () => {
+        it('validates', () => {
+            assert.isNull(validate('scm.getCommitSha.yaml', scm.getCommitSha).error);
+        });
+
+        it('fails', () => {
+            assert.isNotNull(validate('empty.yaml', scm.getCommitSha).error);
+        });
+    });
+
+    describe('updateCommitStatus', () => {
+        it('validates', () => {
+            assert.isNull(validate('scm.updateCommitStatus.yaml', scm.updateCommitStatus).error);
+        });
+
+        it('fails', () => {
+            assert.isNotNull(validate('empty.yaml', scm.updateCommitStatus).error);
+        });
+    });
+});
