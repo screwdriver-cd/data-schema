a404e4801ece95d4b607b4da311d1cb3c79f696c
St. John Johnson <st.john.johnson@gmail.com>
2016-08-28 20:29:47 -0700

    Removing Log Schema (we're not using it)

--- models/index.js
+++ models/index.js
@@ -4,6 +4,5 @@ const build = require('./build');
 const job = require('./job');
 const pipeline = require('./pipeline');
 const user = require('./user');
-const log = require('./log');
 
-module.exports = { build, job, pipeline, user, log };
+module.exports = { build, job, pipeline, user };
