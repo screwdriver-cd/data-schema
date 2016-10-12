6407c8593731be0d8516bde3f674faba2c7c4643
Dao Lam <daolam112@gmail.com>
2016-10-12 15:58:33 -0700

    bitbucket uuid can include {}

--- test/config/regex.test.js
+++ test/config/regex.test.js
@@ -6,7 +6,7 @@ const config = require('../../').config;
 describe('config regex', () => {
     describe('steps', () => {
         it('checks good step names', () => {
-            assert.isNotNull(config.regex.STEP_NAME.test('foo-BAR_15'));
+            assert.isTrue(config.regex.STEP_NAME.test('foo-BAR_15'));
         });
 
         it('fails on bad step names', () => {
@@ -16,7 +16,7 @@ describe('config regex', () => {
 
     describe('jobs', () => {
         it('checks good job names', () => {
-            assert.isNotNull(config.regex.JOB_NAME.test('foo-BAR_15'));
+            assert.isTrue(config.regex.JOB_NAME.test('foo-BAR_15'));
         });
 
         it('fails on bad job names', () => {
@@ -26,12 +26,46 @@ describe('config regex', () => {
 
     describe('environment', () => {
         it('checks good env names', () => {
-            assert.isNotNull(config.regex.ENV_NAME.test('OKAY_YES11'));
+            assert.isTrue(config.regex.ENV_NAME.test('OKAY_YES11'));
         });
 
         it('fails on bad job names', () => {
             assert.isFalse(config.regex.ENV_NAME.test('bad-bad-bad'));
             assert.isFalse(config.regex.ENV_NAME.test('1LOVE'));
+        });
+    });
+
+    describe('checkoutUrl', () => {
+        it('checks good checkout Url', () => {
+            assert.isTrue(
+                config.regex.CHECKOUT_URL.test('https://github.com/screwdriver-cd/data-schema.git'
+            ));
+            assert.isTrue(
+                config.regex.CHECKOUT_URL.test('git@github.com:screwdriver-cd/data-schema.git'
+            ));
+        });
+
+        it('fails on bad checkout Url', () => {
+            assert.isFalse(
+                config.regex.CHECKOUT_URL.test('https://github.com/screwdriver-cd/data-schema'));
+            assert.isFalse(
+                config.regex.CHECKOUT_URL.test('git@screwdriver-cd/data-schema.git'));
+        });
+    });
+
+    describe('scmUri', () => {
+        it('checks good scmUri', () => {
+            assert.isTrue(
+                config.regex.SCM_URI.test('github.com:abc-123:master'
+            ));
+            assert.isTrue(
+                config.regex.SCM_URI.test('bitbucket.org:{123}:master'
+            ));
+        });
+
+        it('fails on bad scmUri', () => {
+            assert.isFalse(config.regex.SCM_URI.test('github.com:master'));
+            assert.isFalse(config.regex.SCM_URI.test('bitbucket.org:{123}'));
         });
     });
 });
