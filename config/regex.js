1758298463b5f464b49bb63e16bf8d70b069bcd3
St. John Johnson <st.john.johnson@gmail.com>
2016-10-28 22:51:42 -0700

    Create .npmignore
    So we don't bundle extra files in our packages
    
    Related to https://github.com/screwdriver-cd/screwdriver/issues/324

--- config/regex.js
+++ config/regex.js
@@ -15,7 +15,7 @@ module.exports = {
     ENV_NAME: /^[A-Z_][A-Z0-9_]*$/,
     // Repo checkout url. For example: https://github.com/screwdriver-cd/data-schema.git#branchName or git@github.com:screwdriver-cd/data-schema.git
     // eslint-disable-next-line max-len
-    CHECKOUT_URL: /^(?:(?:https?|git):\/\/)?(?:[^@]+@)?([^\/:]+)(?:\/|:)([^\/]+)\/([^.#]+)(?:\.git)?(#.+)?$/,
+    CHECKOUT_URL: /^(?:(?:https?|git):\/\/)?(?:[^@]+@)?([^/:]+)(?:\/|:)([^/]+)\/([^.#]+)(?:\.git)?(#.+)?$/,
     // scmUri. For example: github.com:abc-123:master or bitbucket.org:{123}:master
     SCM_URI: /^([^:]+):([^:]+):([^:]+)$/
 };
