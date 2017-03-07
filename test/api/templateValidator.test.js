9e95bbefc22a8e8a302ed69b71bbb6d9a65b780e
Darren M <aeneascorrupt@gmail.com>
2017-03-06 18:15:06 -0800

    feat: adding input & output schema for template validation API endpoint
    There is a feature that aims to add an API endpoint for a user to
    validate their template configuration. This change will introduce a
    schema for both input & output for the API to utilize.

---
+++ test/api/templateValidator.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const assert = require('chai').assert;
+const api = require('../../').api;
+const validate = require('../helper').validate;
+
+describe('api validator', () => {
+    const inputSchema = api.templateValidator.input;
+    const outputSchema = api.templateValidator.output;
+
+    describe('input', () => {
+        it('accepts basic input', () => {
+            assert.isNull(validate('template-validator.input.yaml', inputSchema).error);
+        });
+    });
+
+    describe('output', () => {
+        it('validates basic output', () => {
+            assert.isNull(validate('template-validator.output.yaml', outputSchema).error);
+        });
+
+        it('validates basic output with errors', () => {
+            assert.isNull(validate('template-validator.erroroutput.yaml', outputSchema).error);
+        });
+    });
+});
