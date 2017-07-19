3c53c221390a8cbfea649e6cf2ba2f696ce173e7
Dao Lam <daolam112@gmail.com>
2017-07-19 14:26:06 -0700

    fix: remove schema for delete and create tag

--- test/models/templatetag.test.js
+++ test/models/templatetag.test.js
@@ -10,24 +10,4 @@ describe('model template tag', () => {
             assert.isNull(validate('templatetag.yaml', models.templateTag.base).error);
         });
     });
-
-    describe('create', () => {
-        it('validates the create', () => {
-            assert.isNull(validate('templatetag.create.yaml', models.templateTag.create).error);
-        });
-
-        it('fails the create', () => {
-            assert.isNotNull(validate('empty.yaml', models.templateTag.create).error);
-        });
-    });
-
-    describe('remove', () => {
-        it('validates the remove', () => {
-            assert.isNull(validate('templatetag.remove.yaml', models.templateTag.remove).error);
-        });
-
-        it('fails the remove', () => {
-            assert.isNotNull(validate('empty.yaml', models.templateTag.remove).error);
-        });
-    });
 });
