d8d50b5a478eee13bf0d09257923419c607001d2
Min Zhang <minzhangcmu@gmail.com>
2017-08-07 13:36:17 -0700

    feat: add queue job config validation schema

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,7 @@
 
 const datastore = require('./datastore');
 const executor = require('./executor');
+const queue = require('./queue');
 const scm = require('./scm');
 
-module.exports = { datastore, executor, scm };
+module.exports = { datastore, executor, queue, scm };
