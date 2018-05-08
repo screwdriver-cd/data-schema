ee6204d01870af2cfbf7941e201f66aaeab2e4a9
filbird <filidillidally@gmail.com>
2018-05-08 14:14:23 -0700

    Revert: feat(1041): Add job SCM schema

--- config/job.js
+++ config/job.js
@@ -77,12 +77,6 @@ const SCHEMA_SOURCEPATHS = Joi.alternatives().try(
     Joi.array().items(SCHEMA_SOURCEPATH),
     SCHEMA_SOURCEPATH
 );
-const SCHEMA_REPO = Joi.string().regex(Regex.CHECKOUT_URL);
-const SCHEMA_SCM = Joi.object()
-    .keys({
-        repo: SCHEMA_REPO
-    });
-
 const SCHEMA_JOB = Joi.object()
     .keys({
         annotations: Annotations.annotations,
@@ -91,7 +85,6 @@ const SCHEMA_JOB = Joi.object()
         image: SCHEMA_IMAGE,
         matrix: SCHEMA_MATRIX,
         requires: SCHEMA_REQUIRES,
-        scm: SCHEMA_SCM,
         secrets: SCHEMA_SECRETS,
         settings: SCHEMA_SETTINGS,
         sourcePaths: SCHEMA_SOURCEPATHS,
@@ -112,7 +105,6 @@ module.exports = {
     job: SCHEMA_JOB,
     matrix: SCHEMA_MATRIX,
     requires: SCHEMA_REQUIRES,
-    scm: SCHEMA_SCM,
     secret: SCHEMA_SECRET,
     secrets: SCHEMA_SECRETS,
     settings: SCHEMA_SETTINGS,
