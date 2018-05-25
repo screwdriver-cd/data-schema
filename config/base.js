d95640ade1a20088abd74c8ee8f3623ca276ee3c
filbird <filidillidally@gmail.com>
2018-05-25 11:59:39 -0700

    Add scmUrls schema

--- config/base.js
+++ config/base.js
@@ -11,12 +11,15 @@ const SCHEMA_JOBS = Joi.object()
     // All others are marked as invalid
     .unknown(false);
 const SCHEMA_SHARED = Job.job;
+const SCHEMA_SCM_URL = Joi.string().regex(Regex.CHECKOUT_URL);
+const SCHEMA_SCM_URLS = Joi.array().items(SCHEMA_SCM_URL).min(1);
 const SCHEMA_CONFIG = Joi.object()
     .keys({
         version: Joi.number().integer().min(1).max(50),
         annotations: Annotations.annotations,
         jobs: SCHEMA_JOBS,
-        shared: SCHEMA_SHARED
+        shared: SCHEMA_SHARED,
+        scmUrls: SCHEMA_SCM_URLS
     })
     .requiredKeys('jobs')
     .unknown(false);
@@ -28,5 +31,6 @@ const SCHEMA_CONFIG = Joi.object()
 module.exports = {
     jobs: SCHEMA_JOBS,
     shared: SCHEMA_SHARED,
+    scmUrls: SCHEMA_SCM_URLS,
     config: SCHEMA_CONFIG
 };
