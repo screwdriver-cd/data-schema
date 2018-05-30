5ffef2ee9d4b867ef7cbc6943f6df0abb6c335b7
Tiffany Kyi <tiffanykyi@gmail.com>
2018-05-29 17:56:28 -0700

    fix(653): Only allow ~jobname for blockedBy

--- config/job.js
+++ config/job.js
@@ -79,13 +79,17 @@ const SCHEMA_STEPS = Joi.array().items(SCHEMA_STEP).min(1);
 const SCHEMA_TEMPLATE = Joi.string().regex(Regex.FULL_TEMPLATE_NAME);
 const SCHEMA_JOBNAME = Joi.string().regex(Regex.JOB_NAME);
 const SCHEMA_TRIGGER = Joi.string().regex(Regex.TRIGGER); // ~commit, ~pr, etc.
-const SCHEMA_EXTERNAL_TRIGGER = Joi.string().regex(Regex.EXTERNAL_TRIGGER);
+const SCHEMA_INTERNAL_TRIGGER = Joi.string().regex(Regex.INTERNAL_TRIGGER); // ~main, ~jobOne
+const SCHEMA_EXTERNAL_TRIGGER = Joi.string().regex(Regex.EXTERNAL_TRIGGER); // ~sd@123:main
 const SCHEMA_REQUIRES_VALUE = Joi.alternatives().try(SCHEMA_JOBNAME, SCHEMA_TRIGGER);
 const SCHEMA_REQUIRES = Joi.alternatives().try(
     Joi.array().items(SCHEMA_REQUIRES_VALUE),
     SCHEMA_REQUIRES_VALUE
 );
-const SCHEMA_BLOCKEDBY_VALUE = Joi.alternatives().try(SCHEMA_JOBNAME, SCHEMA_EXTERNAL_TRIGGER);
+const SCHEMA_BLOCKEDBY_VALUE = Joi.alternatives().try(
+    SCHEMA_INTERNAL_TRIGGER,
+    SCHEMA_EXTERNAL_TRIGGER
+);
 const SCHEMA_BLOCKEDBY = Joi.alternatives().try(
     Joi.array().items(SCHEMA_BLOCKEDBY_VALUE),
     SCHEMA_BLOCKEDBY_VALUE
