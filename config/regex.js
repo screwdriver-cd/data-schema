47efb7b24b7e85e808ae0a7e389a0b041c2a5cf3
Tiffany Kyi <tiffanykyi@gmail.com>
2018-07-20 15:16:28 -0700

    fix(1200): Fix git checkout url parsing regex

--- config/regex.js
+++ config/regex.js
@@ -59,7 +59,7 @@ module.exports = {
     ENV_NAME: /^[A-Z_][A-Z0-9_]*$/,
     // Repo checkout url. For example: https://github.com/screwdriver-cd/data-schema.git#branchName or git@github.com:screwdriver-cd/data-schema.git
     // eslint-disable-next-line max-len
-    CHECKOUT_URL: /^(?:(?:https?|git):\/\/)?(?:[^@]+@)?([^/:]+)(?:\/|:)([^/]+)\/(.+?)(?:\.git)?(#.+)?$/,
+    CHECKOUT_URL: /^(?:(?:https:\/\/(?:[^@/:\s]+@)?)|git@)+([^/:\s]+)(?:\/|:)([^/:\s]+)\/([^\s]+?)(?:\.git)?(#[^\s]+)?$/,
     // scmUri. For example: github.com:abc-123:master or bitbucket.org:{123}:master
     SCM_URI: /^([^:]+):([^:]+):([^:]+)$/,
     // Image aliases can only contain A-Z,a-z,0-9,-,_
