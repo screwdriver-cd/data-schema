9282c55dcf7ad1b78b27b1a85b2bcde515813933
Tiffany Kyi <tiffanykyi@gmail.com>
2018-08-09 14:57:16 -0700

    feat(1204): Remove default page and count for pagination

--- api/pagination.js
+++ api/pagination.js
@@ -5,13 +5,11 @@ const Joi = require('joi');
 const MODEL = {
     page: Joi
         .number().integer().min(1)
-        .default(1)
         .description('Page to paginate'),
 
     count: Joi
         .number().integer().min(1)
         .max(50)
-        .default(50)
         .description('Count to paginate'),
 
     sort: Joi
