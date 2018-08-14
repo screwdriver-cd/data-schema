dd4c3fe48dcc56b9aaad1c72ca1bc0c498f16cf7
Tiffany Kyi <tiffanykyi@gmail.com>
2018-08-14 15:27:32 -0700

    fix(1204): Make paginate field optional

--- plugins/datastore.js
+++ plugins/datastore.js
@@ -23,7 +23,7 @@ const SCHEMA_SCAN = Joi.object().keys({
     paginate: Joi.object().keys({
         count: Joi.number().integer().positive().required(),
         page: Joi.number().integer().positive().required()
-    }).required(),
+    }),
     sort: Joi.string().lowercase().valid(['ascending', 'descending']).default('descending')
 });
 
