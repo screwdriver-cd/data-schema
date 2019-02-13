781243dcf00d736e61c8d3a4b51c3ce114a758c6
Pranav Ravichandran <pranavrc@yahoo-inc.com>
2019-02-13 15:39:47 -0800

    fix: Update executor.start schema

--- plugins/executor.js
+++ plugins/executor.js
@@ -5,7 +5,14 @@ const Job = require('../config/job');
 const Joi = require('joi');
 const models = require('../models');
 const buildId = Joi.reach(models.build.base, 'id').required();
+const eventId = Joi.reach(models.event.base, 'id');
 const jobId = Joi.reach(models.job.base, 'id');
+
+const SCHEMA_PIPELINE = Joi.object().keys({
+    id: Joi.reach(models.pipeline.base, 'id').required(),
+    scmContext: Joi.reach(models.pipeline.base, 'scmContext').required()
+}).unknown();
+
 const SCHEMA_START = Joi.object().keys({
     build: Joi.object(),
     jobId,
@@ -13,6 +20,9 @@ const SCHEMA_START = Joi.object().keys({
     blockedBy: Joi.array().items(jobId),
     freezeWindows: Job.freezeWindows,
     buildId,
+    eventId,
+    tokenGen: Joi.func(),
+    pipeline: SCHEMA_PIPELINE,
     buildClusterName: Joi.reach(models.buildCluster.base, 'name'),
     container: Joi.reach(models.build.base, 'container').required(),
     apiUri: Joi.string().uri().required()
--- test/helper.js
+++ test/helper.js
@@ -13,11 +13,16 @@ module.exports = {
      * @method validate
      * @param  {String} filename Filename located in test/data
      * @param  {Joi}    schema   Join schema to validate against
+     * @param  {Object} extend   Object to extend the parsed object with
      * @return {JoiResult}       Result from Joi.Validate
      */
-    validate: (filename, schema) => {
+    validate: (filename, schema, extend) => {
         const exampleFile = path.join(DATA_DIR, filename);
         const example = yaml.safeLoad(fs.readFileSync(exampleFile).toString());
+
+        if (extend !== undefined) {
+            Object.assign(example, extend);
+        }
 
         return joi.validate(example, schema);
     }
