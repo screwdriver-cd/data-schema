34b5c79f744edd0bd897426e2b6b30f8c0f0b7c6
Alan Dong <alandong@stanford.edu>
2019-09-23 14:27:07 -0700

    fix: add parameters to top level of screwdriver.yaml

---
+++ config/parameters.js
@@ -0,0 +1,24 @@
+'use strict';
+const Joi = require('joi');
+
+const SCHEMA_PARAMETERS_STRING = Joi.string();
+const SCHEMA_PARAMETERS_OBJECT = Joi.object({
+    value: Joi.string().required(),
+    description: Joi.string()
+})
+    .options({
+        language: {
+            object: {
+                allowUnknown: 'only supports string or key: { value, description } pair as values'
+            }
+        }
+    });
+
+const SCHEMA_PARAMETERS = Joi.object()
+    .pattern(Joi.any(),
+        Joi.alternatives().try(SCHEMA_PARAMETERS_STRING, SCHEMA_PARAMETERS_OBJECT))
+    .default({});
+
+module.exports = {
+    parameters: SCHEMA_PARAMETERS
+};
