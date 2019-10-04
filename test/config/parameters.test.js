536e2b59b40f85339cd84f42c7d68910bc69f62a
Alan Dong <alandong@stanford.edu>
2019-10-04 10:05:24 -0700

    fix: fix test

--- test/config/parameters.test.js
+++ test/config/parameters.test.js
@@ -3,11 +3,12 @@
 const assert = require('chai').assert;
 const config = require('../../').config;
 const validate = require('../helper').validate;
+const parameters = config.parameters.parameters;
 
 describe('config parameters', () => {
     describe('parameters', () => {
         it('validates parameters', () => {
-            assert.isNull(validate('config.parameters.yaml', config.job.parameters).error);
+            assert.isNull(validate('config.parameters.yaml', parameters).error);
         });
     });
 });
