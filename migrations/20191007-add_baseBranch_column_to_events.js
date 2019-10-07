72630d016be656e63a016deef3df13b05034cda2
Keisuke Kumada <kumada626@users.noreply.github.com>
2019-10-07 10:09:43 +0900

    Create 20191007-add_baseBranch_column_to_events.js

---
+++ migrations/20191007-add_baseBranch_column_to_events.js
@@ -0,0 +1,19 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
+const table = `${prefix}events`;
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.transaction(async (transaction) => {
+            await queryInterface.createTable(table, {
+                baseBranch: {
+                    type: Sequelize.TEXT
+                }
+            }, { transaction }
+            );
+        });
+    }
+};
