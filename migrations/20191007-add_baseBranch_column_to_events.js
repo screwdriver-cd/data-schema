7b10185300c1a7f6cb116f114487192ca1012c35
Keisuke Kumada <kumada626@users.noreply.github.com>
2019-10-09 05:09:27 +0900

    fix(1755): Add baseBranch column to events migration (#358)
    * Add baseBranch column to events init migration
    
    * revert
    
    * Create 20191007-add_baseBranch_column_to_events.js
    
    * fix method

---
+++ migrations/20191007-add_baseBranch_column_to_events.js
@@ -0,0 +1,17 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
+const table = `${prefix}events`;
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.transaction(async (transaction) => {
+            await queryInterface.addColumn(table, 'baseBranch', {
+                type: Sequelize.TEXT
+            }, { transaction }
+            );
+        });
+    }
+};
