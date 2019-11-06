220c30de85bfc8095e8d3bc50460bed0680751f1
parthasl <parthasl@yahoo-inc.com>
2019-11-06 08:19:03 -0800

    feat(1830): fix test

--- test/config/scmContext.test.js
+++ test/config/scmContext.test.js
@@ -4,9 +4,9 @@ const Joi = require('joi');
 const assert = require('chai').assert;
 const config = require('../../').config;
 
-describe('config parameters', () => {
-    describe('parameters', () => {
-        it('validates parameters', () => {
+describe('config scm context', () => {
+    describe('scm context', () => {
+        it('validates scm context', () => {
             assert.isNull(Joi.validate('github:github.com', config.scmContext.name).error);
         });
     });
