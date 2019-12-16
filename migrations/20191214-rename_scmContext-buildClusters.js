3751d7eb707a6e7096e3e6aa43b8e78f6fd3bc86
parthasl <parthasl@yahoo-inc.com>
2019-12-16 09:00:17 -0800

    fix(1881): build cluster to support multple scm conexts

---
+++ migrations/20191214-rename_scmContext-buildClusters.js
@@ -0,0 +1,32 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
+const table = `${prefix}buildClusters`;
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        try {
+            await queryInterface.sequelize.query('SET SQL_MODE = CONCAT(@@SQL_MODE, ' +
+                '\',ANSI_QUOTES\')');
+            // eslint-disable-next-line no-empty
+        } catch (e) {}
+
+        await queryInterface.sequelize.transaction(async (transaction) => {
+            await queryInterface.changeColumn(table, 'scmContext', Sequelize.TEXT('medium'),
+                { transaction });
+            await queryInterface.renameColumn(table, 'scmContext', 'scmContexts',
+                { transaction });
+            await queryInterface.sequelize.query(`UPDATE "${table}"
+                SET "scmContexts" = CONCAT('["', "scmContexts", '"]')`,
+            { transaction });
+        });
+
+        try {
+            await queryInterface.sequelize.query('SET SQL_MODE = REPLACE(@@SQL_MODE, ' +
+                '\'ANSI_QUOTES\', \'\')');
+            // eslint-disable-next-line no-empty
+        } catch (e) {}
+    }
+};
