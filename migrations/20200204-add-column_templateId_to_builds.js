a6c60c088b3768cc790fb1fa3528e9dbb4212aff
InderH <inderbir.h@gmail.com>
2020-02-12 16:12:33 -0800

    feat(1091): change templateId to integer in migration files

--- migrations/20200204-add-column_templateId_to_builds.js
+++ migrations/20200204-add-column_templateId_to_builds.js
@@ -9,7 +9,7 @@ module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.sequelize.transaction(async (transaction) => {
             await queryInterface.addColumn(table, 'templateId', {
-                type: Sequelize.DOUBLE }, { transaction });
+                type: Sequelize.INTEGER }, { transaction });
 
             await queryInterface.addIndex(table, 'templateId', {
                 name: `${table}_template_id` }, { transaction });
