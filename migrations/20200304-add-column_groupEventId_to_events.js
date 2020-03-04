1831f55d3521327ef72c639dc6a70905066d49dc
Tiffany Kyi <tiffanykyi@gmail.com>
2020-03-04 15:21:42 -0800

    feat(1785): Add groupEventId

---
+++ migrations/20200304-add-column_groupEventId_to_events.js
@@ -0,0 +1,18 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
+const table = `${prefix}builds`;
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.transaction(async (transaction) => {
+            await queryInterface.addColumn(table, 'templateId', {
+                type: Sequelize.INTEGER }, { transaction });
+
+            await queryInterface.addIndex(table, ['templateId'], {
+                name: `${table}_template_id`, transaction });
+        });
+    }
+};
