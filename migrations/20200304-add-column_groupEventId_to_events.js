727de420beb50cdbc7ede496a899a4d09f13f490
Tiffany Kyi <tiffanykyi@gmail.com>
2020-03-11 10:49:15 -0700

    fix: Upload correct file

--- migrations/20200304-add-column_groupEventId_to_events.js
+++ migrations/20200304-add-column_groupEventId_to_events.js
@@ -3,16 +3,13 @@
 'use strict';
 
 const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
-const table = `${prefix}builds`;
+const table = `${prefix}events`;
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.sequelize.transaction(async (transaction) => {
-            await queryInterface.addColumn(table, 'templateId', {
-                type: Sequelize.INTEGER }, { transaction });
-
-            await queryInterface.addIndex(table, ['templateId'], {
-                name: `${table}_template_id`, transaction });
+            await queryInterface.addColumn(table, 'groupEventId', {
+                type: Sequelize.DOUBLE }, { transaction });
         });
     }
 };
