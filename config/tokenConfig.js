861ceb73c0766787a6815c842450d8196aa9cbb3
ppaul <pritam.paul@verizonmedia.com>
2020-06-16 20:10:51 -0400

    fix: remove default for isPR property

--- config/tokenConfig.js
+++ config/tokenConfig.js
@@ -8,7 +8,7 @@ const TOKEN_CONFIG = Joi.object({
     buildId: Joi.reach(models.build.base, 'id').required(),
     jobId: Joi.reach(models.job.base, 'id').required(),
     eventId: Joi.reach(models.event.base, 'id'),
-    isPR: Joi.boolean().required().default(true),
+    isPR: Joi.boolean().required(),
     pipelineId: Joi.reach(models.pipeline.base, 'id').required(),
     scmContext: Joi.reach(models.pipeline.base, 'scmContext').required(),
     configPipelineId: Joi.reach(models.pipeline.base, 'id').optional(),
