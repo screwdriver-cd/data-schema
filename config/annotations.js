3460ee41a3280c77bab7ea78b89c90676daa9375
kkawahar <xi.reich8@gmail.com>
2020-10-13 02:54:50 +0900

    fix(2053): add RESERVED_JOB_ANNOTATIONS and RESERVED_PIPELINE_ANNOTATIONS (#414)

--- config/annotations.js
+++ config/annotations.js
@@ -4,11 +4,36 @@ const Joi = require('joi');
 
 // List of data, eventually key-value pairs
 const SCHEMA_ANNOTATIONS = Joi.object().optional();
+// for check warnAnnotations
+const RESERVED_JOB_ANNOTATIONS = [
+    'screwdriver.cd/buildCluster',
+    'screwdriver.cd/buildPeriodically',
+    'screwdriver.cd/collapseBuilds',
+    'screwdriver.cd/cpu',
+    'screwdriver.cd/disk',
+    'screwdriver.cd/diskSpeed',
+    'screwdriver.cd/executor',
+    'screwdriver.cd/ram',
+    'screwdriver.cd/repoManifest',
+    'screwdriver.cd/timeout',
+    'screwdriver.cd/dockerEnabled',
+    'screwdriver.cd/dockerCpu',
+    'screwdriver.cd/dockerRam',
+    'screwdriver.cd/coverageScope'
+];
+const RESERVED_PIPELINE_ANNOTATIONS = [
+    'screwdriver.cd/buildCluster',
+    'screwdriver.cd/restrictPR',
+    'screwdriver.cd/chainPR',
+    'screwdriver.cd/pipelineDescription'
+];
 
 /**
  * The definition of the annotations pieces
  * @type {Object}
  */
 module.exports = {
-    annotations: SCHEMA_ANNOTATIONS
+    annotations: SCHEMA_ANNOTATIONS,
+    reservedJobAnnotations: RESERVED_JOB_ANNOTATIONS,
+    reservedPipelineAnnotations: RESERVED_PIPELINE_ANNOTATIONS
 };
