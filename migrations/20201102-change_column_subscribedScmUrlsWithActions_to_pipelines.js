9ebe3a42d3779de62e8c371ebe06cde74de883ec
Jithin Emmanuel <jithin1987@gmail.com>
2020-11-02 17:04:40 -0800

    fix(1415): fix the migration file (#419)
    * fix(1415): fix the migration file
    
    * fix: lint fix

---
+++ migrations/20201102-change_column_subscribedScmUrlsWithActions_to_pipelines.js
@@ -0,0 +1,15 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
+const table = `${prefix}pipelines`;
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.transaction(async (transaction) => {
+            await queryInterface.changeColumn(table, 'subscribedScmUrlsWithActions', {
+                type: Sequelize.TEXT('medium') }, { transaction });
+        });
+    }
+};
