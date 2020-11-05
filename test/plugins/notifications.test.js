e5feeba261ed8c5457909b2d9235815433a810ef
Kenta Kawaharada <kkawahar@yahoo-corp.jp>
2020-11-05 14:09:41 +0900

    feat: add plugins/notifications

---
+++ test/plugins/notifications.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Joi = require('joi');
+const assert = require('chai').assert;
+const notifications = require('../../plugins/notifications');
+const validate = require('../helper').validate;
+
+describe('notifications test', () => {
+    describe('schemaBuildData', () => {
+        it('validates the schemaBuildData', () => {
+            const schemaBuildData = Joi.object().keys({
+                ...notifications.schemaBuildData
+            });
+
+            assert.isNull(
+                validate('notifications.schemaBuildData.yaml', schemaBuildData).error
+            );
+        });
+
+        it('fails the schemaBuildData for empty yaml', () => {
+            const schemaBuildData = Joi.object().keys({
+                ...notifications.schemaBuildData
+            });
+
+            assert.isNotNull(validate('empty.yaml', schemaBuildData).error);
+        });
+    });
+});
