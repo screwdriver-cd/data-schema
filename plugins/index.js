55af4d888cf410dff69509c9b535393a2a76f509
kkawahar <xi.reich8@gmail.com>
2020-11-11 07:05:25 +0900

    feat: add plugins/notifications (#420)

--- plugins/index.js
+++ plugins/index.js
@@ -2,7 +2,8 @@
 
 const datastore = require('./datastore');
 const executor = require('./executor');
+const notifications = require('./notifications');
 const queue = require('./queue');
 const scm = require('./scm');
 
-module.exports = { datastore, executor, queue, scm };
+module.exports = { datastore, executor, notifications, queue, scm };
