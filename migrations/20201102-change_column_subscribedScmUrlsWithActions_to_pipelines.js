1cc663881362463fc92515493d5fa27f8f1f7e6a
Keisuke Kumada <kumada626@users.noreply.github.com>
2020-12-04 17:31:55 +0900

    Update 20201102-change_column_subscribedScmUrlsWithActions_to_pipelines.js

--- migrations/20201102-change_column_subscribedScmUrlsWithActions_to_pipelines.js
+++ migrations/20201102-change_column_subscribedScmUrlsWithActions_to_pipelines.js
@@ -8,7 +8,11 @@ const table = `${prefix}pipelines`;
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.sequelize.transaction(async (transaction) => {
-            await queryInterface.removeIndex(table, `${table}_subscribed_scm_urls_with_actions`);
+            await queryInterface.removeIndex(
+                table,
+                `${table}_subscribed_scm_urls_with_actions`,
+                { transaction }
+            );
 
             await queryInterface.changeColumn(table, 'subscribedScmUrlsWithActions', {
                 type: Sequelize.TEXT('medium') }, { transaction });
