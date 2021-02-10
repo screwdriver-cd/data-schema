967a04437f207ac15c3c0e2f9a680e2c22b92417
klu909 <klu13960@gmail.com>
2021-02-10 13:32:18 -0800

    fix: wrap table name around double quote

--- migrations/20210122-add_column_latestVersion_to_commands.js
+++ migrations/20210122-add_column_latestVersion_to_commands.js
@@ -11,7 +11,7 @@ module.exports = {
             await queryInterface.addColumn(table, 'latest', {
                 type: Sequelize.BOOLEAN }, { transaction });
             // eslint-disable-next-line max-len
-            await queryInterface.sequelize.query(`UPDATE ${table} SET latest = true WHERE id in (SELECT max(id) FROM (SELECT * FROM ${table}) AS cmd GROUP BY namespace, name)`);
+            await queryInterface.sequelize.query(`UPDATE "${table}" SET latest = true WHERE id in (SELECT max(id) FROM (SELECT * FROM "${table}") AS cmd GROUP BY namespace, name)`);
         });
     }
 };
