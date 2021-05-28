db28af0cfb5ec9ab81ab46fa8bc9af52261f3a04
tkyi <tiffanykyi@gmail.com>
2021-05-28 16:27:21 -0700

    fix: Update checkout command schema with RO headless info

--- plugins/scm.js
+++ plugins/scm.js
@@ -46,7 +46,9 @@ const PARENT_CONFIG = Joi.object().keys({
     host: Joi.string().required(),
     org: Joi.string().required(),
     repo: Joi.string().required(),
-    sha: Joi.string().required()
+    sha: Joi.string().required(),
+    username: Joi.string().optional(),
+    accessToken: Joi.string().optional()
 });
 const GET_CHECKOUT_COMMAND = Joi.object().keys({
     branch: Joi.string().required(),
