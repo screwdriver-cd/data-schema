a77b5e264ac2d2b537d91acad710f00f341d0c5e
dsagar <dsagar@verizonmedia.com>
2021-09-14 09:22:48 -0700

    feat(2346): Making parameters as optional for pipeline

--- config/parameters.js
+++ config/parameters.js
@@ -12,19 +12,17 @@ const SCHEMA_PARAMETERS_OBJECT = Joi.object({
     'object.unknown': 'only supports string or key: {{#label}} pair as values'
 });
 
-const SCHEMA_PARAMETERS_BASE = Joi.object().pattern(
-    Joi.any(),
-    Joi.alternatives().try(
-        SCHEMA_PARAMETERS_STRING,
-        Joi.array().items(SCHEMA_PARAMETERS_STRING),
-        SCHEMA_PARAMETERS_OBJECT
+const SCHEMA_PARAMETERS = Joi.object()
+    .pattern(
+        Joi.any(),
+        Joi.alternatives().try(
+            SCHEMA_PARAMETERS_STRING,
+            Joi.array().items(SCHEMA_PARAMETERS_STRING),
+            SCHEMA_PARAMETERS_OBJECT
+        )
     )
-);
-
-const SCHEMA_PARAMETERS_WITH_DEFAULT = SCHEMA_PARAMETERS_BASE.default({});
-const SCHEMA_PARAMETERS_OPTIONAL = SCHEMA_PARAMETERS_BASE.optional();
+    .optional();
 
 module.exports = {
-    parameters: SCHEMA_PARAMETERS_WITH_DEFAULT,
-    parametersOptional: SCHEMA_PARAMETERS_OPTIONAL
+    parameters: SCHEMA_PARAMETERS
 };
