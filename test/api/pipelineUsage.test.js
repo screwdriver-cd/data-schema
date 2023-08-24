fae6a14f7b98717864fbc20cea65853a025882a8
Nathan Thomas <nathanthomas707@gmail.com>
2023-08-24 10:52:07 -0700

    Add tests

---
+++ test/api/pipelineUsage.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { assert } = require('chai');
+const { api } = require('../..');
+const { validate } = require('../helper');
+
+describe('api get pipelineUsage', () => {
+    const inputSchema = api.pipelineUsage.get;
+
+    describe('basic', () => {
+        it('accepts response', () => {
+            assert.isNull(validate('api.pipelineUsage.yaml', inputSchema).error);
+        });
+
+        it('accepts null lastRun', () => {
+            assert.isNull(validate('api.pipelineUsage.null-date.yaml', inputSchema).error);
+        });
+
+        it('rejects null name', () => {
+            assert.isOk(validate('api.pipelineUsage.null-name.yaml', inputSchema).error);
+        });
+    });
+});
