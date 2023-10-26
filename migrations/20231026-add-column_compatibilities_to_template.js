9b9a0873528d3b62620c94f0f819c90ac013d7f6
Alan Dong <adong@users.noreply.github.com>
2023-10-26 10:51:04 -0700

    fix: add migration files for compatibilities and badges

---
+++ migrations/20231026-add-column_compatibilities_to_template.js
@@ -0,0 +1,21 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const prefix = process.env.DATASTORE_SEQUELIZE_PREFIX || '';
+const table = `${prefix}templates`;
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.addColumn(
+                table,
+                'compatibilities',
+                {
+                    type: Sequelize.TEXT
+                },
+                { transaction }
+            );
+        });
+    }
+};
