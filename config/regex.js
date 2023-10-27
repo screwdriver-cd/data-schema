fd37d39bb9ff2d4606e158a7661a7821a02be190
dsagar <dsagar@verizonmedia.com>
2023-10-27 08:05:05 -0700

    fix(2767): Relax job name Regex to allow stage setup/teardown jobs

--- config/regex.js
+++ config/regex.js
@@ -41,7 +41,8 @@ module.exports = {
     // Steps can only be named with A-Z,a-z,0-9,-,_
     STEP_NAME: /^[\w-]+$/,
     // Jobs can only be named with A-Z,a-z,0-9,-,_
-    JOB_NAME: /^[\w-]+$/,
+    // Also allow stage setup/teardown like stage@stage-name:setup
+    JOB_NAME: /^(([\w-]+)|(stage@[\w-]+:(setup|teardown)))$/,
     // PR JOB Name can only be PR-1 or PR-1:main, group1: PR-prNum, group2: jobName
     PR_JOB_NAME: /^(PR-\d+)(?::([\w-]+))?$/,
     // Match all possible job name
