630d437e4677bcd4e9baa6d01862b5a0d992e123
Dayanand Sagar <sagar1312@gmail.com>
2024-02-26 15:04:20 -0800

    fix(3022): Rename 'screwdriver.cd/virtualTrigger' job annotation to 'screwdriver.cd/virtualJob' (#553)

--- config/annotations.js
+++ config/annotations.js
@@ -27,7 +27,7 @@ const RESERVED_JOB_ANNOTATIONS = [
     'screwdriver.cd/blockedBySameJob',
     'screwdriver.cd/blockedBySameJobWaitTime',
     'screwdriver.cd/jobDisabledByDefault',
-    'screwdriver.cd/virtualTrigger'
+    'screwdriver.cd/virtualJob'
 ];
 const RESERVED_PIPELINE_ANNOTATIONS = [
     'screwdriver.cd/buildCluster',
