418a6c7db3fae97dd4653d445e7522107d4f8691
klu909 <klu13960@gmail.com>
2024-03-14 05:59:40 -0700

    feat(3059): Pipeline Template Enhancements

--- config/base.js
+++ config/base.js
@@ -67,22 +67,21 @@ const SCHEMA_CONFIG_PRE_TEMPLATE_MERGE = Joi.object()
     .keys({
         template: Joi.string().regex(Regex.FULL_TEMPLATE_NAME_WITH_NAMESPACE),
         version: Joi.number().integer().min(1).max(50),
-        annotations: Annotations.annotations.when('template', { is: Joi.exist(), then: Joi.forbidden() }),
+        annotations: Annotations.annotations,
         jobs: SCHEMA_JOBS.when('template', { is: Joi.exist(), then: Joi.forbidden(), otherwise: Joi.required() }),
         shared: Joi.when('template', {
             is: Joi.exist(),
-            then: Joi.object().keys({
-                image: Job.image,
-                environment: Job.environment,
-                settings: Job.settings
+            then: SCHEMA_SHARED.append({
+                order: Joi.forbidden(),
+                steps: Joi.forbidden()
             }),
             otherwise: SCHEMA_SHARED
         }),
-        cache: SCHEMA_CACHE.when('template', { is: Joi.exist(), then: Joi.forbidden() }),
-        childPipelines: SCHEMA_CHILD_PIPELINES.when('template', { is: Joi.exist(), then: Joi.forbidden() }),
+        cache: SCHEMA_CACHE,
+        childPipelines: SCHEMA_CHILD_PIPELINES,
         stages: SCHEMA_STAGES.when('template', { is: Joi.exist(), then: Joi.forbidden() }),
-        subscribe: SCHEMA_SUBSCRIBE.when('template', { is: Joi.exist(), then: Joi.forbidden() }),
-        parameters: Parameters.parameters.default({}).when('template', { is: Joi.exist(), then: Joi.forbidden() })
+        subscribe: SCHEMA_SUBSCRIBE,
+        parameters: Parameters.parameters.default({})
     })
     .unknown(false);
 
