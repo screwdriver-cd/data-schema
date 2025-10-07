f9a9d5827ffd45e337e9f63c166a2a51ee66d6cd
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-10-07 16:05:11 -0700

    Adhere to removeIndex signature

--- migrations/20250912140626-remove-pipelineId-and-createTime-index-from-events.js
+++ migrations/20250912140626-remove-pipelineId-and-createTime-index-from-events.js
@@ -9,8 +9,7 @@ module.exports = {
     // eslint-disable-next-line no-unused-vars
     up: async (queryInterface, Sequelize) => {
         await queryInterface.sequelize.transaction(async transaction => {
-            await queryInterface.removeIndex(table, {
-                name: `${table}_pipeline_id_create_time`,
+            await queryInterface.removeIndex(table, `${table}_pipeline_id_create_time`, {
                 transaction
             });
         });
